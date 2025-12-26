13eadddc511e883dc7bf3ce0c6843532b8597b8c
Fordi Malanda <fordimalanda7@gmail.com>
2025-12-26 13:22:45 +0100

    feat: Fichier de configuration de base de données - Exemple

---
+++ config/database.example.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 
+ * Copiez ce fichier vers database.php et modifiez les valeurs selon votre environnement
+ * 
+ */
+
+return [
+    'driver'   => 'mysql',
+    'host'     => 'localhost',
+    'dbname'   => 'votre_base_de_donnees',
+    'username' => 'root',
+    'password' => '',
+    'charset'  => 'utf8mb4',
+];
